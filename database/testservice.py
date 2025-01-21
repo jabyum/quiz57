f807230c3444d08f12e5a3f76d86ced9aa47d419
jabyum <jabyum@github.com>
2025-01-21 18:14:49 +0500

    second

--- database/testservice.py
+++ database/testservice.py
@@ -10,4 +10,7 @@ def add_question_db(main_question, v1,v2, correct_answer, level,
                                 v1=v1, v2=v2, v3=v3, v4=v4,
                                 correct_answer=correct_answer,
                                 level=level)
+        db.add(new_question)
+        db.commit()
+        return True
 
